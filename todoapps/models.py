9580413edaac097c94f17312a5e8495663ee116b
Ben De Bonis <ben.debonis@gmail.com>
2022-07-12 12:52:11 -0700

    Added Tasks and Entries

--- todoapps/models.py
+++ todoapps/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of a model"""
+        return self.text
+
+class Task(models.Model):
+    """A task part of a topic"""
+    task = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'tasks'
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
